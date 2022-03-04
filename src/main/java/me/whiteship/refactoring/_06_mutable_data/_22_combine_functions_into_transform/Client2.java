3606aee8e922410c0675b266e8d86b3903a9ea22
Jerry(이재헌) <ti6419@gmail.com>
2022-03-04 23:25:55 +0900

    Combine functions into transform

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_22_combine_functions_into_transform/Client2.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_22_combine_functions_into_transform/Client2.java
@@ -3,22 +3,15 @@ package me.whiteship.refactoring._06_mutable_data._22_combine_functions_into_tra
 import java.time.Month;
 import java.time.Year;
 
-public class Client2 {
+public class Client2 extends ReadingClient{
 
     private double base;
     private double taxableCharge;
 
     public Client2(Reading reading) {
-        this.base = baseRate(reading.month(), reading.year()) * reading.quantity();
-        this.taxableCharge = Math.max(0, this.base - taxThreshold(reading.year()));
-    }
-
-    private double taxThreshold(Year year) {
-        return 5;
-    }
-
-    private double baseRate(Month month, Year year) {
-        return 10;
+        EnrichReading enrichReading = enrichReading(reading);
+        this.base = enrichReading.baseCharge();
+        this.taxableCharge = enrichReading.texableCharge();
     }
 
     public double getBase() {
--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_22_combine_functions_into_transform/Reading.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_22_combine_functions_into_transform/Reading.java
@@ -3,5 +3,5 @@ package me.whiteship.refactoring._06_mutable_data._22_combine_functions_into_tra
 import java.time.Month;
 import java.time.Year;
 
-public record Reading(String customer, double quantity, Month month, Year year) {
+public record Reading(String customer, double quantity, Month month, Year year, double texableCharge) {
 }
