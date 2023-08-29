12ce290dc543f9f70b5b67b6c65bf76503fc51de
Lee Jaeheon <ti6419@gmail.com>
2023-08-29 23:31:31 +0900

    Replace primitive with object

--- src/main/java/me/whiteship/refactoring/_11_primitive_obsession/_30_repliace_primitive_with_object/Order.java
+++ src/main/java/me/whiteship/refactoring/_11_primitive_obsession/_30_repliace_primitive_with_object/Order.java
@@ -1,14 +1,12 @@
 package me.whiteship.refactoring._11_primitive_obsession._30_repliace_primitive_with_object;
 
 public class Order {
+	private Priority priority;
 
-    private String priority;
-
-    public Order(String priority) {
-        this.priority = priority;
+    public Order(String priorityValue) {
+        this.priority = new Priority(priorityValue);
     }
-
-    public String getPriority() {
-        return priority;
-    }
+	public Priority getPriority() {
+		return priority;
+	}
 }
