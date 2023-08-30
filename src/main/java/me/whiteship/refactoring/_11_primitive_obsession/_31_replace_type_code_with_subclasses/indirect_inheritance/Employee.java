18c6c596b4a549d2d49677be3ba9e8f593f93686
Lee Jaeheon <ti6419@gmail.com>
2023-08-30 21:09:52 +0900

    Replace type code with subclasses

--- src/main/java/me/whiteship/refactoring/_11_primitive_obsession/_31_replace_type_code_with_subclasses/indirect_inheritance/Employee.java
+++ src/main/java/me/whiteship/refactoring/_11_primitive_obsession/_31_replace_type_code_with_subclasses/indirect_inheritance/Employee.java
@@ -1,35 +1,34 @@
 package me.whiteship.refactoring._11_primitive_obsession._31_replace_type_code_with_subclasses.indirect_inheritance;
 
-import java.util.List;
-
 public class Employee {
 
     private String name;
-
-    private String type;
+	private EmployeeType type;
 
     public Employee(String name, String type) {
-        this.validate(type);
         this.name = name;
-        this.type = type;
+		this.type = this.employeeType(type);
     }
 
-    private void validate(String type) {
-        List<String> legalTypes = List.of("engineer", "manager", "salesman");
-        if (!legalTypes.contains(type)) {
-            throw new IllegalArgumentException(type);
-        }
-    }
+	private EmployeeType employeeType(String type) {
+		return switch (type) {
+			case "engineer" -> new Engineer();
+			case "manager" -> new Manager();
+			case "salesman" -> new Salesman();
+			default -> throw new IllegalArgumentException(type);
+		};
+	}
 
     public String capitalizedType() {
-        return this.type.substring(0, 1).toUpperCase() + this.type.substring(1).toLowerCase();
+		return this.type.capitalizedType();
+
     }
 
     @Override
     public String toString() {
         return "Employee{" +
                 "name='" + name + '\'' +
-                ", type='" + type + '\'' +
+                ", type='" + type.toString() + '\'' +
                 '}';
     }
 }
