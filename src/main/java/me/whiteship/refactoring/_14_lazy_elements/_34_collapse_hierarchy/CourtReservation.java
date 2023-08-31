8d6495e1684365684ebd58a18c4a47d374496db0
Lee Jaeheon <ti6419@gmail.com>
2023-08-31 22:52:05 +0900

    Collapse hierachy

--- src/main/java/me/whiteship/refactoring/_14_lazy_elements/_34_collapse_hierarchy/CourtReservation.java
+++ src/main/java/me/whiteship/refactoring/_14_lazy_elements/_34_collapse_hierarchy/CourtReservation.java
@@ -1,7 +1,24 @@
 package me.whiteship.refactoring._14_lazy_elements._34_collapse_hierarchy;
 
-public class CourtReservation extends Reservation {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class CourtReservation {
 
     private String courtNumber;
 
+	private LocalDateTime startDateTime;
+	private LocalDateTime endDateTime;
+	private List<String> members;
+	private String owner;
+	private boolean paid;
+
+	public CourtReservation(String courtNumber, LocalDateTime startDateTime, LocalDateTime endDateTime, List<String> members, String owner, boolean paid) {
+		this.courtNumber = courtNumber;
+		this.startDateTime = startDateTime;
+		this.endDateTime = endDateTime;
+		this.members = members;
+		this.owner = owner;
+		this.paid = paid;
+	}
 }
