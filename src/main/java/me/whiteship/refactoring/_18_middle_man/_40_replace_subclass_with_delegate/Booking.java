be224133d0489c0ded02b949e69821f200583446
Lee Jaeheon <ti6419@gmail.com>
2023-09-01 22:23:32 +0900

    Replace subclass with delegate

--- src/main/java/me/whiteship/refactoring/_18_middle_man/_40_replace_subclass_with_delegate/Booking.java
+++ src/main/java/me/whiteship/refactoring/_18_middle_man/_40_replace_subclass_with_delegate/Booking.java
@@ -5,28 +5,43 @@ import java.time.LocalDateTime;
 
 public class Booking {
 
-    protected Show show;
+	protected Show show;
 
-    protected LocalDateTime time;
+	protected LocalDateTime time;
+	protected PremiumDelegate premiumDelegate;
 
-    public Booking(Show show, LocalDateTime time) {
-        this.show = show;
-        this.time = time;
-    }
+	public Booking(Show show, LocalDateTime time) {
+		this.show = show;
+		this.time = time;
+	}
 
-    public boolean hasTalkback() {
-        return this.show.hasOwnProperty("talkback") && !this.isPeakDay();
-    }
+	public static Booking createBooking(Show show, LocalDateTime time) {
+		return new Booking(show, time);
+	}
 
-    protected boolean isPeakDay() {
-        DayOfWeek dayOfWeek = this.time.getDayOfWeek();
-        return dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY;
-    }
+	public static Booking createPremiumBooking(Show show, LocalDateTime time, PremiumExtra extra) {
+		Booking booking = createBooking(show, time);
+		booking.premiumDelegate = new PremiumDelegate(booking, extra);
+		return booking;
+	}
 
-    public double basePrice() {
-        double result = this.show.getPrice();
-        if (this.isPeakDay()) result += Math.round(result * 0.15);
-        return result;
-    }
+	public boolean hasTalkback() {
+		return (this.premiumDelegate != null) ? this.premiumDelegate.hasTalkback() : this.show.hasOwnProperty("talkback") && !this.isPeakDay();
+	}
+
+	protected boolean isPeakDay() {
+		DayOfWeek dayOfWeek = this.time.getDayOfWeek();
+		return dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY;
+	}
+
+	public double basePrice() {
+		double result = this.show.getPrice();
+		if (this.isPeakDay()) result += Math.round(result * 0.15);
+		return result;
+	}
+
+	public boolean hasDinner() {
+		return (this.premiumDelegate != null) && this.premiumDelegate.hasDinner();
+	}
 
 }
