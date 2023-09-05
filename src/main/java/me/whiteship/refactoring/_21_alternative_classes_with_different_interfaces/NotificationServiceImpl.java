314f96265b22d7d1a4c9330cf665a37d9a69f0f0
Lee Jaeheon <ti6419@gmail.com>
2023-09-05 21:08:24 +0900

    Alternative classes with different interfaces

---
+++ src/main/java/me/whiteship/refactoring/_21_alternative_classes_with_different_interfaces/NotificationServiceImpl.java
@@ -0,0 +1,12 @@
+package me.whiteship.refactoring._21_alternative_classes_with_different_interfaces;
+
+public class NotificationServiceImpl implements NotificationService {
+	private AlertService alertService;
+	@Override
+	public void sendNotification(Notification notification) {
+		AlertMessage alertMessage = new AlertMessage();
+		alertMessage.setMessage(notification.getTitle());
+		alertMessage.setFor(notification.getReceiver());
+		alertService.add(alertMessage);
+	}
+}
